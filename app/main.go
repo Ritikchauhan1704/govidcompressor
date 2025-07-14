2c10bbbc88e7bba1528504349cdd32dd1c5b9e23
RitikChauhan1704 <critik1704@gmail.com>
2025-07-14 23:28:51 +0530

    a

--- app/main.go
+++ app/main.go
@@ -1,7 +1,58 @@
-package main;
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
 
-func main(){
-	fmt.Println("hello world")
+func compressVideo(inputPath, outputPath string) error {
+	// ffmpeg command:
+	// -vcodec libx264: use H.264 encoder
+	// -crf 28: lower = better quality, higher = more compression
+	cmd := exec.Command("ffmpeg", "-i", inputPath, "-vcodec", "libx264", "-crf", "28", outputPath)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func main() {
+	// change this to your video folder path
+	videoDir := "./videos"
+	outputDir := "./compressed"
+
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = filepath.Walk(videoDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// skip folders
+		if info.IsDir() {
+			return nil
+		}
+
+		// process only video files
+		ext := filepath.Ext(path)
+		if ext == ".mp4" || ext == ".mov" || ext == ".mkv" {
+			outputPath := filepath.Join(outputDir, info.Name())
+			fmt.Println("Compressing:", path)
+			err := compressVideo(path, outputPath)
+			if err != nil {
+				log.Printf("Error compressing %s: %v", path, err)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
